08b8e3954c5e2686923e38ddb3d3c7ac48446416
Alexander Gent <aagent298@gmail.com>
2026-05-30 23:45:52 +1000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
+
 cp -v -f SimpleHTTPServerWithUpload.py /bin
-cp -v -f SimpleHTTPServerWithUpload.sh /bin
-cp -v -f SimpleHTTPServerWithUpload.service /lib/systemd/system
+
+cat <<'EOF'>/bin/SimpleHTTPServerWithUpload.sh
+#!/bin/bash
+clear
+cd /mnt/shared_media
+python3 /bin/SimpleHTTPServerWithUpload.py 8080
+EOF
+
+cat <<'EOF'>/lib/systemd/system/SimpleHTTPServerWithUpload.service
+[Unit]
+Description=Simple HTTP Server With Upload
+
+[Service]
+ExecStart=/bin/SimpleHTTPServerWithUpload.sh
+Restart=Always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
 chmod -v +x /bin/SimpleHTTPServerWithUpload.py
 chmod -v +x /bin/SimpleHTTPServerWithUpload.sh
 chmod -v 644 /lib/systemd/system/SimpleHTTPServerWithUpload.service
+
 systemctl enable SimpleHTTPServerWithUpload
 exit
